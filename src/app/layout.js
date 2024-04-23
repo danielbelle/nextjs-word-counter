98bb27b72e8c9fd42b5acd2508a2aa6eca42059b
danielbelle <henrique.danielb@gmail.com>
2024-04-23 11:59:03 -0300

    new logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/src/app/favicon.ico" />
+      </Head>
       <body className={inter.className + " bg-[#fff] m-0"}>{children}</body>
     </html>
   );
